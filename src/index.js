5f36307bb81737ae4260745c7c7390c7fcb041e2
Parker Parham <parkerparham@gmail.com>
2022-08-06 12:19:38 -0400

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,80 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+//get toy data
+function getToyData(){
+  fetch("http://localhost:3000/toys")
+    .then((response) => response.json())
+    .then((toyData => toyData.forEach((toy) => {
+      // console.log(toy)
+      renderCard(toy)
+    })))
+}
+getToyData()
+
+function buyToy(toy){
+  console.log(JSON.stringify(toy))
+  fetch("http://localhost:3000/toys",{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': "application/json"
+    },
+    body:JSON.stringify(toy)
+  })
+  .then((response) => response.json())
+}
+
+
+//build card out
+function renderCard(toy){
+  let div = document.createElement('div')
+  const toyCollection = document.getElementById('toy-collection')
+  div.classList.add("card")
+  toyCollection.appendChild(div)
+  div.innerHTML =  `
+    <h2>${toy.name}</h2>
+    <img src="${toy.image}" class="toy-avatar" />
+    <p><span>${toy.likes}</span> likes</p>
+    <button class="like-btn"  id="${toy.id}">Like ❤️</button>
+  `
+  let button = div.getElementsByClassName("like-btn")
+  button[0].addEventListener('click', () => {
+    toy.likes+= 1
+    div.querySelector('span').textContent = toy.likes
+    updateLikes(toy)
+  })
+  // updateLikes()
+}
+
+//Event handlers
+function handleSubmit(e){
+    e.preventDefault()
+    let toyObj = {
+      name:e.target.name.value,
+      image:e.target.image.value,
+      likes: 0
+    }
+    renderCard(toyObj)
+    buyToy(toyObj)
+}
+
+//event listeners
+let toyForm = document.getElementsByClassName("add-toy-form")
+// console.log(toyForm)
+toyForm[0].addEventListener(`submit`, handleSubmit)
+
+
+function updateLikes(toyObj){
+  fetch(`http://localhost:3000/toys/${toyObj.id}`,{
+    method: 'PATCH',
+    headers:{
+      'Content-Type': 'application/json',
+      Accept: "application/json"
+    },
+    body: JSON.stringify(toyObj)
+  })
+  .then(res => res.json())
+  .then(toyObj => console.log(toyObj))
+}
